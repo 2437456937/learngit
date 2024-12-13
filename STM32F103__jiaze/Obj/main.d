21356342bdf16b87448a8585d918c79b01e72520
Your Name <email@example.com>
2024-12-13 12:44:25 +0800

    仅测试显示屏

--- STM32F103__jiaze/Obj/main.d
+++ STM32F103__jiaze/Obj/main.d
@@ -16,10 +16,7 @@
 .\obj\main.o: .\FWlib\inc\stm32f10x_tim.h
 .\obj\main.o: .\FWlib\inc\stm32f10x_usart.h
 .\obj\main.o: .\FWlib\inc\misc.h
+.\obj\main.o: user\sys.h
 .\obj\main.o: user\delay.h
 .\obj\main.o: D:\keil\ARM\ARMCC\Bin\..\include\string.h
-.\obj\main.o: user\relay.h
-.\obj\main.o: user\ad_get.h
-.\obj\main.o: user\sys.h
 .\obj\main.o: user\tm1638.h
-.\obj\main.o: user\show.h
